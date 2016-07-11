ad560cf8a27fecc72a9d6c160c816c2b81ef4eb3
Sam Bryant <srbry@hotmail.com>
2016-07-11 14:52:41 +0100

    add test deps
    Signed-off-by: Jim Conner <jim@secret.org.uk>

---
+++ vendor/github.com/cloudfoundry-community/gogobosh/api_test.go
@@ -0,0 +1,201 @@
+package gogobosh_test
+
+import (
+	. "github.com/cloudfoundry-community/gogobosh"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Api", func() {
+	Describe("Test API", func() {
+		var client *Client
+
+		Describe("Test get stemcells", func() {
+			BeforeEach(func() {
+				setup(MockRoute{"GET", "/stemcells", stemcells, ""}, "basic")
+				config := &Config{
+					BOSHAddress: server.URL,
+					Username:    "admin",
+					Password:    "admin",
+				}
+
+				client, _ = NewClient(config)
+			})
+
+			AfterEach(func() {
+				teardown()
+			})
+
+			It("can get stemcells", func() {
+				stemcells, err := client.GetStemcells()
+				Expect(err).Should(BeNil())
+				Expect(stemcells[0].Name).Should(Equal("bosh-warden-boshlite-ubuntu-trusty-go_agent"))
+				Expect(stemcells[0].OperatingSystem).Should(Equal("ubuntu-trusty"))
+				Expect(stemcells[0].Version).Should(Equal("3126"))
+				Expect(stemcells[0].CID).Should(Equal("c3705a0d-0dd3-4b67-52b5-50533a432244"))
+			})
+		})
+
+		Describe("Test get releases", func() {
+			BeforeEach(func() {
+				setup(MockRoute{"GET", "/releases", releases, ""}, "basic")
+				config := &Config{
+					BOSHAddress: server.URL,
+					Username:    "admin",
+					Password:    "admin",
+				}
+
+				client, _ = NewClient(config)
+			})
+
+			AfterEach(func() {
+				teardown()
+			})
+
+			It("can get releases", func() {
+				releases, err := client.GetReleases()
+				Expect(err).Should(BeNil())
+				Expect(releases[0].Name).Should(Equal("bosh-warden-cpi"))
+				Expect(releases[0].ReleaseVersions[0].Version).Should(Equal("28"))
+				Expect(releases[0].ReleaseVersions[0].CommitHash).Should(Equal("4c36884a"))
+				Expect(releases[0].ReleaseVersions[0].UncommittedChanges).Should(Equal(false))
+				Expect(releases[0].ReleaseVersions[0].CurrentlyDeployed).Should(Equal(true))
+
+			})
+		})
+
+		Describe("Test deployments", func() {
+			Describe("get deployments", func() {
+				BeforeEach(func() {
+					setup(MockRoute{"GET", "/deployments", deployments, ""}, "basic")
+					config := &Config{
+						BOSHAddress: server.URL,
+						Username:    "admin",
+						Password:    "admin",
+					}
+
+					client, _ = NewClient(config)
+				})
+
+				AfterEach(func() {
+					teardown()
+				})
+
+				It("can get deployments", func() {
+					deployments, err := client.GetDeployments()
+					Expect(err).Should(BeNil())
+					Expect(deployments[0].Name).Should(Equal("cf-warden"))
+					Expect(deployments[0].CloudConfig).Should(Equal("none"))
+					Expect(deployments[0].Releases[0].Name).Should(Equal("cf"))
+					Expect(deployments[0].Releases[0].Version).Should(Equal("223"))
+					Expect(deployments[0].Stemcells[0].Name).Should(Equal("bosh-warden-boshlite-ubuntu-trusty-go_agent"))
+					Expect(deployments[0].Stemcells[0].Version).Should(Equal("3126"))
+				})
+			})
+
+			Describe("create deployments", func() {
+				BeforeEach(func() {
+					setup(MockRoute{"POST", "/deployments", deploymentTask, ""}, "basic")
+					config := &Config{
+						BOSHAddress: server.URL,
+						Username:    "admin",
+						Password:    "admin",
+					}
+
+					client, _ = NewClient(config)
+				})
+
+				AfterEach(func() {
+					teardown()
+				})
+
+				It("can create deployments", func() {
+					task, err := client.CreateDeployment("---\nname: foo")
+					Expect(err).Should(BeNil())
+					Expect(task.ID).Should(Equal(2))
+				})
+			})
+		})
+
+		Describe("Test tasks", func() {
+			BeforeEach(func() {
+				setup(MockRoute{"GET", "/tasks", tasks, ""}, "basic")
+				config := &Config{
+					BOSHAddress: server.URL,
+					Username:    "admin",
+					Password:    "admin",
+				}
+
+				client, _ = NewClient(config)
+			})
+
+			AfterEach(func() {
+				teardown()
+			})
+
+			It("can get tasks", func() {
+				tasks, err := client.GetTasks()
+				Expect(err).Should(BeNil())
+				Expect(tasks[0].ID).Should(Equal(1180))
+				Expect(tasks[0].State).Should(Equal("processing"))
+				Expect(tasks[0].Description).Should(Equal("run errand acceptance_tests from deployment cf-warden"))
+			})
+		})
+
+		Describe("Test get deployment manifest", func() {
+			BeforeEach(func() {
+				setup(MockRoute{"GET", "/deployments/foo", manifest, ""}, "basic")
+				config := &Config{
+					BOSHAddress: server.URL,
+					Username:    "admin",
+					Password:    "admin",
+				}
+
+				client, _ = NewClient(config)
+			})
+
+			AfterEach(func() {
+				teardown()
+			})
+
+			It("can get deployments manifest", func() {
+				manifest, err := client.GetDeployment("foo")
+				Expect(err).Should(BeNil())
+				Expect(manifest.Manifest).Should(Equal("---\nfoo: bar\n"))
+			})
+		})
+
+		Describe("Test get deployment vms", func() {
+			BeforeEach(func() {
+				setupMultiple([]MockRoute{
+					{"GET", "/deployments/foo/vms", "", server.URL + "/tasks/2"},
+					{"GET", "/tasks/2", task, ""},
+					{"GET", "/tasks/2", task, ""},
+					{"GET", "/tasks/2/output", vms, ""},
+				}, "basic")
+
+				config := &Config{
+					BOSHAddress: server.URL,
+					Username:    "admin",
+					Password:    "admin",
+				}
+
+				client, _ = NewClient(config)
+			})
+
+			AfterEach(func() {
+				teardown()
+			})
+
+			It("can get deployments vms", func() {
+				vms, err := client.GetDeploymentVMs("foo")
+				Expect(err).Should(BeNil())
+				Expect(vms[0].VMCID).Should(Equal("ec974048-3352-4ba4-669d-beab87b16bcb"))
+				Expect(vms[0].IPs[0]).Should(Equal("10.244.0.142"))
+				Expect(vms[0].AgentID).Should(Equal("c5e7c705-459e-41c0-b640-db32d8dc6e71"))
+				Expect(vms[0].JobName).Should(Equal("doppler_z1"))
+			})
+		})
+
+	})
+})
---
+++ vendor/github.com/cloudfoundry-community/gogobosh/client.go
@@ -0,0 +1,293 @@
+package gogobosh
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+//Client used to communicate with BOSH
+type Client struct {
+	config   Config
+	Endpoint Endpoint
+}
+
+//Config is used to configure the creation of a client
+type Config struct {
+	BOSHAddress       string
+	Port              string
+	Username          string
+	Password          string
+	UAAAuth           bool
+	HttpClient        *http.Client
+	SkipSslValidation bool
+	TokenSource       oauth2.TokenSource
+}
+
+type Endpoint struct {
+	URL string `json:"doppler_logging_endpoint"`
+}
+
+// request is used to help build up a request
+type request struct {
+	method string
+	url    string
+	header map[string]string
+	params url.Values
+	body   io.Reader
+	obj    interface{}
+}
+
+//DefaultConfig configuration for client
+func DefaultConfig() *Config {
+	return &Config{
+		BOSHAddress:       "https://192.168.50.4:25555",
+		Username:          "admin",
+		Password:          "admin",
+		HttpClient:        http.DefaultClient,
+		SkipSslValidation: true,
+	}
+}
+
+func DefaultEndpoint() *Endpoint {
+	return &Endpoint{
+		URL: "https://192.168.50.4:8443",
+	}
+}
+
+// NewClient returns a new client
+func NewClient(config *Config) (*Client, error) {
+	// bootstrap the config
+	defConfig := DefaultConfig()
+
+	if len(config.BOSHAddress) == 0 {
+		config.BOSHAddress = defConfig.BOSHAddress
+	}
+
+	if len(config.Username) == 0 {
+		config.Username = defConfig.Username
+	}
+
+	if len(config.Password) == 0 {
+		config.Password = defConfig.Password
+	}
+
+	endpoint := &Endpoint{}
+	config.HttpClient = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: config.SkipSslValidation,
+			},
+		},
+	}
+	authType, err := getAuthType(config.BOSHAddress, config.HttpClient)
+	if err != nil {
+		return nil, fmt.Errorf("Could not get auth type: %v", err)
+	}
+	if authType != "uaa" {
+		config.HttpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			if len(via) > 10 {
+				return fmt.Errorf("stopped after 10 redirects")
+			}
+			req.URL.Host = strings.TrimPrefix(config.BOSHAddress, req.URL.Scheme+"://")
+			req.SetBasicAuth(config.Username, config.Password)
+			req.Header.Add("User-Agent", "gogo-bosh")
+			req.Header.Del("Referer")
+			return nil
+		}
+	} else {
+		ctx := oauth2.NoContext
+		if config.SkipSslValidation == false {
+			ctx = context.WithValue(ctx, oauth2.HTTPClient, defConfig.HttpClient)
+		} else {
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			ctx = context.WithValue(ctx, oauth2.HTTPClient, &http.Client{Transport: tr})
+		}
+
+		endpoint, err := getUAAEndpoint(config.BOSHAddress, oauth2.NewClient(ctx, nil))
+
+		if err != nil {
+			return nil, fmt.Errorf("Could not get api /info: %v", err)
+		}
+
+		authConfig := &oauth2.Config{
+			ClientID: "bosh_cli",
+			Scopes:   []string{""},
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  endpoint.URL + "/oauth/authorize",
+				TokenURL: endpoint.URL + "/oauth/token",
+			},
+		}
+
+		token, err := authConfig.PasswordCredentialsToken(ctx, config.Username, config.Password)
+		if err != nil {
+			return nil, fmt.Errorf("Error getting token: %v", err)
+		}
+
+		config.TokenSource = authConfig.TokenSource(ctx, token)
+		config.HttpClient = oauth2.NewClient(ctx, config.TokenSource)
+
+		config.HttpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			if len(via) > 10 {
+				return fmt.Errorf("stopped after 10 redirects")
+			}
+			req.URL.Host = strings.TrimPrefix(config.BOSHAddress, req.URL.Scheme+"://")
+			req.Header.Add("User-Agent", "gogo-bosh")
+			req.Header.Del("Referer")
+			return nil
+		}
+	}
+	client := &Client{
+		config:   *config,
+		Endpoint: *endpoint,
+	}
+
+	return client, nil
+}
+
+func getAuthType(api string, httpClient *http.Client) (string, error) {
+	info, err := getInfo(api, httpClient)
+	return info.UserAuthenication.Type, err
+}
+
+func getInfo(api string, httpClient *http.Client) (*Info, error) {
+	var (
+		info Info
+	)
+
+	if api == "" {
+		return &Info{}, nil
+	}
+
+	resp, err := httpClient.Get(api + "/info")
+	if err != nil {
+		log.Printf("Error requesting info %v", err)
+		return &Info{}, err
+	}
+	defer resp.Body.Close()
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading info request %v", resBody)
+		return &Info{}, err
+	}
+	err = json.Unmarshal(resBody, &info)
+	return &info, err
+}
+
+func getUAAEndpoint(api string, httpClient *http.Client) (*Endpoint, error) {
+	if api == "" {
+		return DefaultEndpoint(), nil
+	}
+	info, err := getInfo(api, httpClient)
+	URL := info.UserAuthenication.Options.URL
+	return &Endpoint{URL: URL}, err
+}
+
+// NewRequest is used to create a new request
+func (c *Client) NewRequest(method, path string) *request {
+	r := &request{
+		method: method,
+		url:    c.config.BOSHAddress + path,
+		params: make(map[string][]string),
+		header: make(map[string]string),
+	}
+	return r
+}
+
+// DoRequest runs a request with our client
+func (c *Client) DoRequest(r *request) (*http.Response, error) {
+	req, err := r.toHTTP()
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range r.header {
+		req.Header.Add(key, value)
+	}
+	req.SetBasicAuth(c.config.Username, c.config.Password)
+	req.Header.Add("User-Agent", "gogo-bosh")
+	resp, err := c.config.HttpClient.Do(req)
+	return resp, err
+}
+
+// UUID return uuid
+func (c *Client) UUID() string {
+	info, _ := c.GetInfo()
+	return info.UUID
+}
+
+// GetInfo returns BOSH Info
+func (c *Client) GetInfo() (info Info, err error) {
+	r := c.NewRequest("GET", "/info")
+	resp, err := c.DoRequest(r)
+
+	if err != nil {
+		log.Printf("Error requesting info %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading info request %v", resBody)
+		return
+	}
+	err = json.Unmarshal(resBody, &info)
+	if err != nil {
+		log.Printf("Error unmarshaling info %v", err)
+		return
+	}
+	return
+}
+
+// toHTTP converts the request to an HTTP request
+func (r *request) toHTTP() (*http.Request, error) {
+
+	// Check if we should encode the body
+	if r.body == nil && r.obj != nil {
+		if b, err := encodeBody(r.obj); err != nil {
+			return nil, err
+		} else {
+			r.body = b
+		}
+	}
+
+	// Create the HTTP request
+	return http.NewRequest(r.method, r.url, r.body)
+}
+
+func (c *Client) GetToken() (string, error) {
+	token, err := c.config.TokenSource.Token()
+	if err != nil {
+		return "", fmt.Errorf("Error getting bearer token: %v", err)
+	}
+	return "bearer " + token.AccessToken, nil
+}
+
+// decodeBody is used to JSON decode a body
+func decodeBody(resp *http.Response, out interface{}) error {
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	return dec.Decode(out)
+}
+
+// encodeBody is used to encode a request body
+func encodeBody(obj interface{}) (io.Reader, error) {
+	buf := bytes.NewBuffer(nil)
+	enc := json.NewEncoder(buf)
+	if err := enc.Encode(obj); err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
