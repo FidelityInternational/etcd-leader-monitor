1c16b14578216cf367c5de39c034309607244ca9
Sam Bryant <srbry@hotmail.com>
2016-07-14 08:46:02 +0100

    add documentation comments

--- bosh/bosh.go
+++ bosh/bosh.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 )
 
+// FindDeployment - takes deployments and a regex to return the first matching deployment name
 func FindDeployment(deployments []gogobosh.Deployment, regex string) string {
 	for _, deployment := range deployments {
 		matched, _ := regexp.MatchString(regex, deployment.Name)
@@ -15,6 +16,7 @@ func FindDeployment(deployments []gogobosh.Deployment, regex string) string {
 	return ""
 }
 
+// FindVMs - takes an array of VMs and a regex to filter on, returning a new array of all matching vms
 func FindVMs(deploymentVMs []gogobosh.VM, regex string) []gogobosh.VM {
 	var matchedVMs []gogobosh.VM
 	for _, deploymentVM := range deploymentVMs {
